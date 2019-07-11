7136c72b53929ace1948131fa7468bd3752fd766
kencyke <casek2703@gmail.com>
2019-07-12 01:28:29 +0900

    update preinstalls.sh

--- scripts/preinstalls.sh
+++ scripts/preinstalls.sh
@@ -4,7 +4,7 @@ INSTALL () {
     for arg in "$@"
     do
         if [ $(dpkg-query -W -f='${Status}' $arg 2>/dev/null | grep "ok installed") -eq 0 ]; then
-        apt-get install -y $arg;
+        apt-get install -y --no-install-recommends $arg;
         fi
     done
 }
@@ -13,14 +13,17 @@ set -x
 
 apt-get update
 
-## vim, gedit, apt-transport-https
-INSTALL vim gedit gedit-plugins apt-transport-https
+## vim, gedit, apt-transport-https, curl, tar
+INSTALL vim gedit gedit-plugins apt-transport-https curl tar
 
-## https://github.com/tensorflow/swift/blob/master/Installation.md
+## cf. https://github.com/tensorflow/swift/blob/master/Installation.md
 INSTALL clang libpython-dev libblocksruntime-dev
 
-## https://github.com/apple/swift#linux (except clang & libpython-dev)
+## cf. https://github.com/apple/swift#linux (except clang & libpython-dev)
 INSTALL git cmake ninja-build python uuid-dev libicu-dev icu-devtools libedit-dev libxml2-dev libsqlite3-dev swig libncurses5-dev pkg-config libcurl4-openssl-dev systemtap-sdt-dev tzdata rsync
 
-## https://github.com/pyenv/pyenv/wiki/Common-build-problems (except git, libncursesw5-dev & libsqlite3-dev)
-INSTALL make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+## cf. https://github.com/pyenv/pyenv/wiki/Common-build-problems (except curl, git, libncursesw5-dev & libsqlite3-dev)
+INSTALL make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev wget llvm libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+
+## cf. https://github.com/docker-library/golang/blob/master/Dockerfile-debian.template (except make)
+INSTALL g++ gcc libc6-dev pkg-config
